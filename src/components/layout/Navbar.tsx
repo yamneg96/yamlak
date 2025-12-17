15fb1740f0411b29ef99e708dd7e488b16d70cba
Yamlak Negash <yamlaknegash96@gmail.com>
2025-12-17 20:03:54 +0300

    Added the Profile Picture

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,11 +2,15 @@
 
 import Link from "next/link";
 import ThemeToggle from "../ui/ThemeToggle";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center px-6 py-4 border-b">
-      <Link href="/" className="font-bold text-xl cp">Yamlak Negash</Link>
+      <div className="flex items-center justify-center gap-3">
+        <Image src="/favicon.ico" className="rounded-full h-10 w-10" alt="Profile Picture" width={40} height={40} />
+        <Link href="/" className="font-bold text-xl cp">Yamlak Negash</Link>
+      </div>
       <div className="flex gap-6 items-center">
         <Link href="/projects" className="cp">Projects</Link>
         <Link href="/admin" className="cp">Admin</Link>
